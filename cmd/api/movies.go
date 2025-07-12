f4bf894e933059f47164d897d8e27021d9cbc640
thienel <lethien.dev@gmail.com>
2025-07-12 21:33:01 +0700

    feat(movies): decode json and apply validation in create movie handler

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -4,11 +4,41 @@ import (
 	"fmt"
 	"net/http"
 	"thienel/greenlight/internal/data"
+	"thienel/greenlight/internal/validator"
 	"time"
 )
 
 func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "create a new movie")
+	var input struct {
+		Title   string       `json:"title"`
+		Year    int32        `json:"year"`
+		Runtime data.Runtime `json:"runtime"`
+		Genres  []string     `json:"genres"`
+	}
+
+	err := app.readJSON(w, r, &input)
+	if err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	movie := &data.Movie{
+		Title:   input.Title,
+		Year:    input.Year,
+		Runtime: input.Runtime,
+		Genres:  input.Genres,
+	}
+
+	v := validator.New()
+
+	data.ValidateMovie(v, movie)
+
+	if !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
+		return
+	}
+
+	fmt.Fprintf(w, "%+v\n", movie)
 }
 
 func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
