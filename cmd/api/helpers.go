dd24d01b47c175ab4ebc85bc7234f6caedf97a1d
thienel <lethien.dev@gmail.com>
2025-07-12 17:02:32 +0700

    feat(hepers): envelope data in JSON response

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -20,8 +20,11 @@ func (app *application) readIDParam(r *http.Request) (int64, error) {
 	return id, nil
 }
 
-func (app *application) writeJSON(w http.ResponseWriter, status int, data interface{}, headers http.Header) error {
-	js, err := json.Marshal(data)
+type envelope map[string]any
+
+func (app *application) writeJSON(w http.ResponseWriter, status int, data envelope,
+	headers http.Header) error {
+	js, err := json.MarshalIndent(data, "", "  ")
 	if err != nil {
 		return err
 	}
