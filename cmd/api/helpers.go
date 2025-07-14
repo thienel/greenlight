2b9f3cbf3d3718182e3687cc89f495d279c67368
thienel <lethien.dev@gmail.com>
2025-07-14 19:34:51 +0700

    feat(helpers): create query param parsers

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -6,8 +6,10 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"net/url"
 	"strconv"
 	"strings"
+	"thienel/greenlight/internal/validator"
 
 	"github.com/julienschmidt/httprouter"
 )
@@ -95,4 +97,43 @@ func (app *application) readJSON(w http.ResponseWriter, r *http.Request, dst any
 
 func (app *application) badRequestResponse(w http.ResponseWriter, r *http.Request, err error) {
 	app.errorResponse(w, r, http.StatusBadRequest, err.Error())
+}
+
+func (app *application) readString(qs url.Values, key string,
+	defaultValue string) string {
+	s := qs.Get(key)
+
+	if s == "" {
+		return defaultValue
+	}
+
+	return s
+}
+
+func (app *application) readCSV(sq url.Values, key string,
+	defaultValue []string) []string {
+	csv := sq.Get(key)
+
+	if csv == "" {
+		return defaultValue
+	}
+
+	return strings.Split(csv, ",")
+}
+
+func (app *application) readInt(qs url.Values, key string,
+	defaultValue int, v *validator.Validator) int {
+	s := qs.Get(key)
+
+	if s == "" {
+		return defaultValue
+	}
+
+	i, err := strconv.Atoi(s)
+	if err != nil {
+		v.AddError(key, "must be an integer value")
+		return defaultValue
+	}
+
+	return i
 }
