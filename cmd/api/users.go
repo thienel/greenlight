b34d627eb261952f0d99f41b4792d18f0c2377db
thienel <lethien.dev@gmail.com>
2025-07-23 14:02:32 +0700

    feat(users): run send welcome email in background for faster response

--- cmd/api/users.go
+++ cmd/api/users.go
@@ -51,13 +51,15 @@ func (app *application) registerUserHandler(w http.ResponseWriter, r *http.Reque
 		return
 	}
 
-	err = app.mailer.Send(user.Email, "user_welcome.tmpl", user)
-	if err != nil {
-		app.serverErrorResponse(w, r, err)
-		return
-	}
+	go func() {
 
-	err = app.writeJSON(w, http.StatusCreated, envelope{"user": user}, nil)
+		err = app.mailer.Send(user.Email, "user_welcome.tmpl", user)
+		if err != nil {
+			app.logger.PrintError(err, nil)
+		}
+	}()
+
+	err = app.writeJSON(w, http.StatusAccepted, envelope{"user": user}, nil)
 	if err != nil {
 		app.serverErrorResponse(w, r, err)
 	}
