63bf205a613d9f6cac0ab871e4073d09d7ceded0
thienel <lethien.dev@gmail.com>
2025-07-26 16:27:43 +0700

    feat(middleware): add enable CORS middleware and also add CORS safe list configuration in main

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -5,6 +5,7 @@ import (
 	"database/sql"
 	"flag"
 	"os"
+	"strings"
 	"sync"
 	"thienel/greenlight/internal/data"
 	"thienel/greenlight/internal/jsonlog"
@@ -36,6 +37,9 @@ type config struct {
 		username string
 		password string
 		sender   string
+	}
+	cors struct {
+		trustedOrigins []string
 	}
 }
 
@@ -73,6 +77,12 @@ func main() {
 	flag.StringVar(&cfg.smtp.password, "smtp-password", "957bd121724c73", "SMTP password")
 	flag.StringVar(&cfg.smtp.sender, "smtp-sender", "Greenlight <lethien.dev@gmail.com>",
 		"SMTP sender")
+
+	flag.Func("cors-trusted-origins", "Trusted CORS origins (space separated)",
+		func(val string) error {
+			cfg.cors.trustedOrigins = strings.Fields(val)
+			return nil
+		})
 
 	flag.Parse()
 
